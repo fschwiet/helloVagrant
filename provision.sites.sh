0921d5e9fad4a82b0916953dec9c598ab0309f2a
Frank <fschwiet@gmail.com>
2014-05-27 11:57:42 -0700

    have a dedicated wwwuser

--- provision.sites.sh
+++ provision.sites.sh
@@ -5,5 +5,5 @@
 
 if [ $(ps -e -o cmd | grep pm2 | grep hello-server | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
-	cd /sites/www; /usr/local/bin/pm2 start hello-server.js --user vagrant
+	cd /sites/www; /usr/local/bin/pm2 start hello-server.js --user wwwuser
 fi
